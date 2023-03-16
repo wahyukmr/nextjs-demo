f0351046349bc383fc2f1003ee75da1061d531f2
wahyukmr <maryu9229@gmail.com>
2023-03-16 12:08:29 +0700

    setup .env

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    env: {
+        NEXT_PUBLIC_USER: process.env.NEXT_PUBLIC_USER,
+        NEXT_PUBLIC_PASSWORD: process.env.NEXT_PUBLIC_PASSWORD,
+        NEXT_PUBLIC_COLLECTION: process.env.NEXT_PUBLIC_COLLECTION,
+    },
 };
 
 module.exports = nextConfig;
